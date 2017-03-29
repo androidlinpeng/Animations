99e659e9a95cb6b7f7a57e915277210ee2ad3869
linpengdiao <linpd@msgcopy.com>
2017-03-29 14:45:10 +0800

    分支代码更新

---
+++ app/src/main/java/com/msgcopy/application/SemicircleView.java
@@ -0,0 +1,25 @@
+package com.msgcopy.application;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by liang on 2017/3/29.
+ */
+
+public class SemicircleView extends View{
+    
+    public SemicircleView(Context context) {
+        super(context);
+    }
+
+    public SemicircleView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SemicircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
